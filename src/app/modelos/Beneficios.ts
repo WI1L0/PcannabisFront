09261ac03e0784803c15c74410755d89cb13d021
maiigeo <marilyn.mendieta.est@tecazuay.edu.ec>
2023-06-09 16:14:17 -0500

    Front con modelos
    modelitos

---
+++ src/app/modelos/Beneficios.ts
@@ -0,0 +1,10 @@
+
+import {Empresas}from './Empresas';
+export class Beneficios{
+    idBeneficios?: number;
+    tituloBeneficios?: string;
+    descripcionBeneficios?: string;
+    fotoBeneficios?: string;
+    estBeneficios?: boolean;
+    empresas?: Empresas;
+}
---
+++ src/app/modelos/Usuarios.ts
@@ -0,0 +1,9 @@
+import {Empleados} from './Empleados';
+export class Usuarios{
+    idUsuarios?: number;
+    nombreUsuarios?:string;
+    passwordUsuarios?:string;
+    fotoUsuarios?:string;
+    estUsuarios?:boolean;
+    empleados?:Empleados;
+}
