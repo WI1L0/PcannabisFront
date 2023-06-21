5d5cf4298a5bac9cc27648591a49107173be5c59
WI1L0 <william.martinez.est@tecazuay.edu.ec>
2023-06-21 17:56:43 -0500

    uwuw
    ujjs

--- src/app/paginas/control-noticias/control-noticias.component.ts
+++ src/app/paginas/control-noticias/control-noticias.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Noticias } from 'src/app/modelos/Noticias';
 import { AllScriptsService } from 'src/app/scripts/all-scripts.service';
+import { noticias } from 'src/app/services/s-noticias.service';
 
 @Component({
   selector: 'app-control-noticias',
@@ -8,12 +10,34 @@ import { AllScriptsService } from 'src/app/scripts/all-scripts.service';
 })
 export class ControlNoticiasComponent  implements OnInit {
 
+  public pageAct!: number;
+  public listNoticias: Noticias [] = [];
+
   //implementar js en los componentes
-  constructor(private AllScripts:AllScriptsService){
+  constructor(private AllScripts:AllScriptsService, private noticiasServices: noticias){
     AllScripts.Cargar(["default/controlNoticias"]);
   }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+   this.obtenerNoticias();
+ }
+
+ obtenerNoticias() {
+   this.noticiasServices.getNoticias().subscribe(
+     data => {
+       this.listNoticias = data.map(
+         result => {
+           let n = new Noticias;
+           n.idNoticias=result.idNoticias;
+           n.tituloNoticias=result.tituloNoticias;
+           n.descripcionNoticias=result.descripcionNoticias;
+           n.fechaNoticias=result.fechaNoticias;
+           return n;
+         }
+       );
+      }
+     )
+   }
+ 
 
 }
