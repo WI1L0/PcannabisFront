a962d5dd8464aced0477c5c23e45b17dcb32863a
maiigeo <marilyn.mendieta.est@tecazuay.edu.ec>
2023-07-05 09:28:33 -0500

    Responsive
    Responsive

--- src/app/default/footer/footer.component.ts
+++ src/app/default/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { AllScriptsService } from 'src/app/scripts/all-scripts.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit{
 
